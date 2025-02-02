453d6a3b705a11607f75becd3b7837698a188d6c
lcc <LukaszSobczak@gmail.com>
2025-02-02 15:17:16 +0100

    testing rabbit

--- complete/src/main/java/com/example/quiz/chat/GeneralController.java
+++ complete/src/main/java/com/example/quiz/chat/GeneralController.java
@@ -1,15 +1,24 @@
 package com.example.quiz.chat;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GeneralController {
 
-    @GetMapping("")
-    public String index() {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Unnamed");
         return "roomchoice";
     }
+
+    @PostMapping("/")
+    public String roomChoice(Model model) {
+        return "room";
+    }
+
 }
 
 
